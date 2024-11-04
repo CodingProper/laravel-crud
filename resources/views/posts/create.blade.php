d67a5235a8f08af2a1a6731cf1408affb84708a7
CodingProper <muhamedaliwf@mail.ru>
2024-11-04 06:27:57 +0600

    32:30

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,35 +6,23 @@
 @section('content')
     <div class="row">
         <div class="col-lg-6 mx-auto">
-    <form>
+    <form method="POST" action="{{route('posts.store')}}">
+        @csrf
         <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <label for="post-title">Название</label>
+            <input type="text" name="title" class="form-control" id="post-title">
         </div>
+
         <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
+            <label for="post-description">Описание</label>
+            <textarea class="form-control" name="description" id="post-description" rows="3"></textarea>
         </div>
+
         <div class="form-group">
-            <label for="exampleFormControlSelect2">Example multiple select</label>
-            <select multiple class="form-control" id="exampleFormControlSelect2">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
+            <label for="post-price">Цена</label>
+            <input type="text" class="form-control" name="price" id="post-price" >
         </div>
-        <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
+        <button type="submit" class="btn btn-success">Добавить пост</button>
     </form>
         </div>
     </div>
